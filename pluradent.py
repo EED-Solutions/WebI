5926d7b68c80737ce34dce359933f6b81c6639dc
EED85 <eric.brahmann@eed-solutions.de>
2018-04-27 13:49:39 +0200

    reading all subcategories and storing them in csv

--- pluradent.py
+++ pluradent.py
@@ -117,13 +117,12 @@ with open('url_soup.txt','w') as f:
 #    </ol>
 #            </li>
 
-
+#read maincatogeries
 all_main_cat = soup.find_all("a",{"class":"level-top"})
-print(all_main_cat[1])
-l2 = all_main_cat[1].find_all("label")
-b = all_main_cat[1].find_all("span",{"class":"label"})
-b[0].text
-l2[0]
+#print(all_main_cat[1])
+#l2 = all_main_cat[1].find_all("label")
+#b = all_main_cat[1].find_all("span",{"class":"label"})
+
 
 I = 0
 for item in all_main_cat:
@@ -139,9 +138,69 @@ for item in all_main_cat:
     if is_cat == 1:
         print(cat)
         
-    
-    
+#read subcatogeries
+all_subcat = soup.find_all("div",{"class":"category-teasers--wrapper--teaser--content--subcategories mouse-over"})  
 
+l = []
+l2 = []
+I = 0
+J = 0
+for item in all_subcat:
+    I = I +1
+#    print (I)
+    print(I,item.text)
+    print(item)
+    sublinks = item.find_all("a")
+
+    for  link in sublinks:
+        if I >= 1:
+            d={}
+            J = J +1
+            print (J)
+            has_link = link.has_attr('href')
+            print('Hat link',has_link)
+            if link.has_attr('href'):
+                print (link.text,link['href'])
+                d["index"] = I
+                d["index2"] = J
+                d["url"] = link['href']
+                d["Kategorie"] = link.text
+                
+                l.append(d)
+        elif I == 0:
+            d2 = {}
+            d2["index"] = I
+            d2["index2"] = J
+            d2["Kategorie"] = link.text 
+            d2["url"] = ""
+            l2.append(d)
+#exporting            
+df = pandas.DataFrame(l)
+df.to_csv('subcategories.csv') 
+I = 0
+for index,row in df.iterrows():
+    I = I+1
+    print (I)
+    print(row["Kategorie"])
+    if I == 2:
+        
+   
+     
+#df2 = pandas.DataFrame(l2)
+#df2.to_csv('categories.csv')         
+#f = all_subcat[1].find_all("a")
+#f2 = f[1]
+#print(f2.has_attr('href'))
+#f2['href']
+#f3 = f2.find_all(href=True)
+#
+#href_tags = soup.find_all(href=True)
+#f3[0]
+
+
+
+
+#<div class="category-teasers--wrapper--teaser--content--subcategories mouse-over">
 
 all = soup.find_all("div",{"class":"category-subnav--content--list--entry--link"})
 all = soup.find_all("div",{"class":"category-subnav--content--list--entry"})
