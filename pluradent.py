d9a87cc18511378a6d1bddc6fbdd710cb0ba2137
EED85 <eric.brahmann@eed-solutions.de>
2018-04-27 11:13:52 +0200

    trying to read out all main categories

--- pluradent.py
+++ pluradent.py
@@ -30,22 +30,107 @@ print(L)
 
 #developing area
 
+import requests
+requests.__file__
+import bs4
+bs4.__file__
+from bs4 import BeautifulSoup
+
+#reading website
+#cd "C:/Users/ericb/EED-Solutions by Eric Brahmann/Ideal Dental - Code/webI/"
 
 url = 'https://shop.pluradent.de/praxisbedarf.html'
 r = requests.get(url)
 c = r.content
-print(c)
+soup = BeautifulSoup(c,"html.parser")
 
 
+#exporting website to txt
 with open('url.txt','w') as f:
     f.write(str(c))
 
-
-
-soup = BeautifulSoup(c,"html.parser")
 with open('url_soup.txt','w') as f:
     f.write(str(soup))
 
+#with open('url_soup.txt','r') as f:
+#        soup_str = f.read()
+
+        
+#category-subnav--content--list--entry--link
+#<li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link " href="https://shop.pluradent.de/praxisbedarf.html">
+#                Praxisbedarf            </a>
+#                <ol class="category-subnav--content--list">
+#        <li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link category-subnav--content--list--entry--link-active" href="https://shop.pluradent.de/praxisbedarf/pluline-qualitaetsprodukte.html">
+#                PluLine Qualitätsprodukte            </a>
+#                <ol class="category-subnav--content--list">
+#        <li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link " href="https://shop.pluradent.de/praxisbedarf/pluline-qualitaetsprodukte/praxismaterial.html">
+#                Praxismaterial            </a>
+#                    </li>
+#    </ol>
+#        <ol class="category-subnav--content--list">
+#        <li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link " href="https://shop.pluradent.de/praxisbedarf/pluline-qualitaetsprodukte/praxisinstrumente.html">
+#                Praxisinstrumente            </a>
+#                    </li>
+#    </ol>
+#        <ol class="category-subnav--content--list">
+#        <li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link " href="https://shop.pluradent.de/praxisbedarf/pluline-qualitaetsprodukte/kleingeraete.html">
+#                Kleingeräte            </a>
+#                    </li>
+#    </ol>
+#        <ol class="category-subnav--content--list">
+#        <li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link " href="https://shop.pluradent.de/praxisbedarf/pluline-qualitaetsprodukte/ersatzteile-praxis.html">
+#                Ersatzteile Praxis (1)            </a>
+#                    </li>
+#    </ol>
+#            </li>
+#    </ol>
+#        <ol class="category-subnav--content--list">
+#        <li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link " href="https://shop.pluradent.de/praxisbedarf/praxismaterial.html">
+#                Praxismaterial            </a>
+#                    </li>
+#    </ol>
+#        <ol class="category-subnav--content--list">
+#        <li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link " href="https://shop.pluradent.de/praxisbedarf/praxisinstrumente.html">
+#                Praxisinstrumente            </a>
+#                    </li>
+#    </ol>
+#        <ol class="category-subnav--content--list">
+#        <li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link " href="https://shop.pluradent.de/praxisbedarf/kleingeraete.html">
+#                Kleingeräte            </a>
+#                    </li>
+#    </ol>
+#        <ol class="category-subnav--content--list">
+#        <li class="category-subnav--content--list--entry">
+#            <a class="category-subnav--content--list--entry--link " href="https://shop.pluradent.de/praxisbedarf/ersatzteile-praxis.html">
+#                Ersatzteile Praxis (1851)            </a>
+#                    </li>
+#    </ol>
+#            </li>
+
+
+all_main_cat = soup.find_all("a",{"class":"level-top"})
+print(all_main_cat[0].text)
+l2 = all_main_cat[0].find_all("span",{"class":"label"})
+
+
+for item in all_main_cat:
+    label = item.find_all("span",{"class":"label"})
+    print(label[0])
+    
+    
+
+
+all = soup.find_all("div",{"class":"category-subnav--content--list--entry--link"})
+all = soup.find_all("div",{"class":"category-subnav--content--list--entry"})
 
 
 
