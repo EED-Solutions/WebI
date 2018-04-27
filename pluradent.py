23b92645d9a051f5bb87d13e955419006fead9fe
EED85 <eric.brahmann@eed-solutions.de>
2018-04-27 10:01:47 +0200

    first basic function implemented

--- pluradent.py
+++ pluradent.py
@@ -1,2 +1,18 @@
 #This programm will read out article numbers from the pluradent webshop
-url = 'https://shop.pluradent.de/'
+def scrap_pluradent(web = 1):
+    #returns 1, if no error
+    if web == 1:
+        print("web1")
+        url = 'https://shop.pluradent.de/'
+    else:
+        print("web0")
+        url = 'n.a.'
+    
+    print(url)
+    return "1"
+
+L = scrap_pluradent(1)
+print(L)
+
+
+
